40bcc6861021eedbe7eb893e2074fbcc509e5593
Johan Larsson <johan_gavle1@hotmail.com>
2021-05-17 15:40:26 +0200

    Tweak sync.

--- Sideways.Tests/Sync.cs
+++ Sideways.Tests/Sync.cs
@@ -11,20 +11,20 @@
         {
             var source = new FileInfo("D:\\Database.sqlite3");
             var target = Database.DbFile;
-            if (File.Exists(target.FullName))
+            if (File.Exists(source.FullName) &&
+                File.Exists(target.FullName))
             {
+                if (Sideways.Sync.CountDays(symbol, source) > Sideways.Sync.CountDays(symbol, target))
+                {
+                    Sideways.Sync.CopyDays(symbol, source, target);
+                    Sideways.Sync.CopySplits(symbol, source, target);
+                    Sideways.Sync.CopyDividends(symbol, source, target);
+                }
+
                 if (Sideways.Sync.CountMinutes(symbol, source) > Sideways.Sync.CountMinutes(symbol, target))
                 {
                     Sideways.Sync.CopyMinutes(symbol, source, target);
                 }
-
-                if (Sideways.Sync.CountDays(symbol, source) > Sideways.Sync.CountDays(symbol, target))
-                {
-                    Sideways.Sync.CopyDays(symbol, source, target);
-                }
-
-                Sideways.Sync.CopySplits(symbol, source, target);
-                Sideways.Sync.CopyDividends(symbol, source, target);
             }
         }
     }
