ce20ffd096a6d6c6a727a113ddc6b04dbb57a7ff
JohanLarsson <johan_gavle1@hotmail.com>
2021-05-25 13:10:12 +0200

    SImplify drawing of background.

--- Sideways/Chart/ChartBackground.cs
+++ Sideways/Chart/ChartBackground.cs
@@ -9,71 +9,32 @@
         protected override void OnRender(DrawingContext drawingContext)
         {
             var size = this.RenderSize;
-            var candleWidth = this.CandleWidth;
             var candles = this.Candles;
             switch (this.CandleInterval)
             {
                 case CandleInterval.Hour or CandleInterval.Minute:
-                    for (var i = 0; i < Math.Min(candles.Count, Math.Ceiling(size.Width / candleWidth)); i++)
-                    {
-                        var candle = candles[i];
-                        if (TradingDay.IsPreMarket(candle.Time))
-                        {
-                            var p1 = new Point(X(i), 0);
-                            Skip(c => TradingDay.IsPreMarket(c.Time));
-                            drawingContext.DrawRectangle(
-                                Brushes.PreMarket,
-                                null,
-                                new Rect(
-                                    p1,
-                                    new Point(X(i + 1), size.Height)));
-                        }
-                        else if (TradingDay.IsPostMarket(candle.Time))
-                        {
-                            var p1 = new Point(X(i), 0);
-                            Skip(c => TradingDay.IsPostMarket(c.Time));
-
-                            drawingContext.DrawRectangle(
-                                Brushes.PostMarket,
-                                null,
-                                new Rect(
-                                    p1,
-                                    new Point(X(i + 1), size.Height)));
-                        }
-
-                        void Skip(Func<Candle, bool> selector)
-                        {
-                            i++;
-                            while (i < candles.Count - 1 &&
-                                   selector(candles[i + 1]))
-                            {
-                                i++;
-                            }
-                        }
-
-                        double X(int index) => Math.Max(0, size.Width - (index * candleWidth));
-                    }
-
+                    DrawBand(x => TradingDay.IsPreMarket(x.Time), Brushes.PreMarket);
+                    DrawBand(x => TradingDay.IsPostMarket(x.Time), Brushes.PostMarket);
                     break;
                 case CandleInterval.Day:
-                    DrawEven(x => x.Time.Month);
+                    DrawBand(x => x.Time.Month % 2 == 0, Brushes.Even);
                     break;
                 case CandleInterval.Week:
-                    DrawEven(x => x.Time.Year);
+                    DrawBand(x => x.Time.Year % 2 == 0, Brushes.Even);
                     break;
             }
 
-            void DrawEven(Func<Candle, int> func)
+            void DrawBand(Func<Candle, bool> func, SolidColorBrush brush)
             {
                 var position = CandlePosition.Create(this.RenderSize, this.CandleWidth, default);
                 for (var i = 0; i < candles.Count; i++)
                 {
-                    if (func(candles[i]) % 2 == 0)
+                    if (func(candles[i]))
                     {
                         var p1 = new Point(position.Right, size.Height);
 
                         while (i < candles.Count - 1 &&
-                               func(candles[i]) % 2 == 0)
+                               func(candles[i]))
                         {
                             i++;
                             position = position.ShiftLeft();
@@ -84,7 +45,7 @@
                         }
 
                         drawingContext.DrawRectangle(
-                            Brushes.Even,
+                            brush,
                             null,
                             new Rect(
                                 p1,
@@ -96,8 +57,6 @@
                     {
                         break;
                     }
-
-                    double X(int index) => Math.Max(0, size.Width - (index * candleWidth));
                 }
             }
         }
