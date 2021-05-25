8b9e0cd14ba25ef5eaed16f91e62baa5ee268c38
Johan Larsson <johan_gavle1@hotmail.com>
2021-05-25 19:13:10 +0200

    Desaturate some more, maybe too much.

--- Sideways/Chart/Brushes.cs
+++ Sideways/Chart/Brushes.cs
@@ -4,8 +4,13 @@
 
     public static class Brushes
     {
-        public static readonly SolidColorBrush Increasing = Create(Color.FromArgb(byte.MaxValue, 66, 154, 66));
-        public static readonly SolidColorBrush Decreasing = Create(Color.FromArgb(byte.MaxValue, 154, 66, 66));
+        public static readonly SolidColorBrush Increasing = Create(Color.FromArgb(byte.MaxValue, 61, 143, 61));
+        public static readonly SolidColorBrush Decreasing = Create(Color.FromArgb(byte.MaxValue, 143, 61, 61));
+        public static readonly SolidColorBrush Purple = Create(Color.FromArgb(byte.MaxValue, 99, 61, 143));
+        public static readonly SolidColorBrush Yellow = Create(Color.FromArgb(byte.MaxValue, 143, 139, 61));
+        public static readonly SolidColorBrush Orange = Create(Color.FromArgb(byte.MaxValue, 143, 113, 61));
+        public static readonly SolidColorBrush Red = Create(Color.FromArgb(byte.MaxValue, 143, 61, 61));
+        public static readonly SolidColorBrush Green = Create(Color.FromArgb(byte.MaxValue, 68, 143, 61));
         public static readonly SolidColorBrush PreMarket = Create(Color.FromArgb(10, 255, 255, 20));
         public static readonly SolidColorBrush PostMarket = Create(Color.FromArgb(10, 50, 50, 255));
         public static readonly SolidColorBrush Even = Create(Color.FromArgb(10, 80, 80, 80));
