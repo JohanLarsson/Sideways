75f35bd671fc74a46739505391cee8da3b910687
JohanLarsson <johan_gavle1@hotmail.com>
2021-06-02 11:00:51 +0200

    Use CandlePosition in TimeTickBar

--- Sideways/Chart/TimeTickBar.cs
+++ Sideways/Chart/TimeTickBar.cs
@@ -36,8 +36,6 @@
         {
             if (this.Candles is { Count: > 10 } candles)
             {
-                var candleWidth = this.CandleWidth;
-                var actualWidth = this.ActualWidth;
                 var typeface = new Typeface(
                     TextElement.GetFontFamily(this),
                     TextElement.GetFontStyle(this),
@@ -45,26 +43,32 @@
                     TextElement.GetFontStretch(this));
                 var fontSize = TextElement.GetFontSize(this);
                 var fill = this.Fill;
-                var max = Math.Min(candles.Count, actualWidth / candleWidth) - 1;
+                var position = CandlePosition.Create(this.RenderSize, this.CandleWidth, default);
 
                 switch (this.CandleInterval)
                 {
                     case CandleInterval.Week:
-                        for (var i = 0; i < max; i++)
+                        for (var i = 0; i < this.Candles.Count - 1; i++)
                         {
                             if (candles[i].Time.Year != candles[i + 1].Time.Year)
                             {
-                                DrawText(candles[i].Time.Year.ToString(CultureInfo.InvariantCulture), actualWidth - (i * candleWidth));
+                                DrawText(candles[i].Time.Year.ToString(CultureInfo.InvariantCulture), position.Left);
+                            }
+
+                            position = position.ShiftLeft();
+                            if (position.Left < 0)
+                            {
+                                break;
                             }
                         }
 
                         break;
                     case CandleInterval.Day:
-                        for (var i = 0; i < max; i++)
+                        for (var i = 0; i < this.Candles.Count - 1; i++)
                         {
                             if (candles[i].Time.Month != candles[i + 1].Time.Month)
                             {
-                                DrawText(TickText(candles[i].Time), actualWidth - (i * candleWidth));
+                                DrawText(TickText(candles[i].Time), position.Left);
 
                                 static string TickText(DateTimeOffset time)
                                 {
@@ -75,33 +79,51 @@
                                     };
                                 }
                             }
+
+                            position = position.ShiftLeft();
+                            if (position.Left < 0)
+                            {
+                                break;
+                            }
                         }
 
                         break;
                     case CandleInterval.Hour:
-                        for (var i = 0; i < max; i++)
+                        foreach (var candle in this.Candles)
                         {
-                            if (HourAndMinute.EndOfHourCandle(candles[i].Time) == new HourAndMinute(10, 00))
+                            if (HourAndMinute.EndOfHourCandle(candle.Time) == new HourAndMinute(10, 00))
                             {
-                                DrawText(candles[i].Time.Day.ToString(CultureInfo.InvariantCulture), actualWidth - (i * candleWidth));
+                                DrawText(candle.Time.Day.ToString(CultureInfo.InvariantCulture), position.Left);
+                            }
+
+                            position = position.ShiftLeft();
+                            if (position.Left < 0)
+                            {
+                                break;
                             }
                         }
 
                         break;
                     case CandleInterval.Minute:
-                        for (var i = 0; i < max; i++)
+                        foreach (var candle in this.Candles)
                         {
-                            switch (candles[i].Time)
+                            switch (candle.Time)
                             {
                                 case { Hour: 9, Minute: 31 }:
-                                    DrawText("09:30", actualWidth - (i * candleWidth));
+                                    DrawText("09:30", position.Left);
                                     break;
                                 case { Hour: 12 }:
-                                    DrawText("12:00", actualWidth - (i * candleWidth));
+                                    DrawText("12:00", position.Left);
                                     break;
                                 case { Hour: 15 }:
-                                    DrawText("15:00", actualWidth - (i * candleWidth));
+                                    DrawText("15:00", position.Left);
                                     break;
+                            }
+
+                            position = position.ShiftLeft();
+                            if (position.Left < 0)
+                            {
+                                break;
                             }
                         }
 
