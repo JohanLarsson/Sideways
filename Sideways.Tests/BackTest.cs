c7689fd96836b4cbd4d17db5395207493d24e155
JohanLarsson <johan_gavle1@hotmail.com>
2021-06-07 08:03:30 +0200

    Use RelativeVolume and RelativeClose

--- Sideways.Tests/BackTest.cs
+++ Sideways.Tests/BackTest.cs
@@ -18,13 +18,11 @@ namespace Sideways.Tests
         [TestCaseSource(nameof(Symbols))]
         public static void GapUp(string symbol)
         {
-            const double gap = 0.10;
-            Console.WriteLine($"{symbol} {100 * gap}% gap ups. Buy at open sell at close three days later.");
             var candles = Database.ReadDays(symbol);
             for (var i = 1; i < candles.Count - 5; i++)
             {
                 var candle = candles[i];
-                if (Gap() > gap)
+                if (Gap() > 0.10)
                 {
                     Console.WriteLine($"{candle.Time:yyyy-MM-dd} {100 * (candles[i + 5].Close - candle.Open) / candle.Open,6:F1}%");
                 }
@@ -39,8 +37,6 @@ namespace Sideways.Tests
         [Test]
         public static void AllGapUps()
         {
-            const double minGap = 0.1;
-            Console.WriteLine("symbol;date;gap;relative_volume;relative_close;three_day;five_day;link");
             var bookmarks = new List<Bookmark>();
             foreach (var symbol in Symbols)
             {
@@ -48,10 +44,11 @@ namespace Sideways.Tests
                 for (var i = 10; i < candles.Count - 20; i++)
                 {
                     var candle = candles[i];
-                    if (Percent(candles[i - 1].High, candle.Open) > minGap &&
-                        candle.Close * candle.Volume > 10_000_000)
+                    if (Percent(candles[i - 1].High, candle.Open) > 0.1 &&
+                        candle.Close * candle.Volume > 10_000_000 &&
+                        RelativeClose() > 0 &&
+                        RelativeVolume() > 0)
                     {
-                        // Console.WriteLine($"{symbol};{candle.Time:yyyy-MM-dd};{gap};{RelativeVolume()};{RelativeClose()};{Percent(candle.Open, candles[i + 3].Close)};{Percent(candle.Open, candles[i + 5].Close)};https://www.tradingview.com/chart/?symbol={symbol}&interval=1D&date={candle.Time:yyyy-MM-dd}");
                         bookmarks.Add(new Bookmark(symbol, candle.Time, ImmutableSortedSet<string>.Empty, null));
                     }
 
@@ -60,23 +57,23 @@ namespace Sideways.Tests
                         return (end - start) / start;
                     }
 
-                    ////double RelativeVolume()
-                    ////{
-                    ////    return candle.Volume / Slice(candles, i - 10, i - 1).Average(x => x.Volume);
-                    ////}
+                    double RelativeVolume()
+                    {
+                        return candle.Volume / Slice(candles, i - 10, i - 1).Average(x => x.Volume);
+                    }
 
-                    ////double RelativeClose()
-                    ////{
-                    ////    return (candle.Close - candle.Low) / (candle.High - candle.Low);
-                    ////}
+                    double RelativeClose()
+                    {
+                        return (candle.Close - candle.Low) / (candle.High - candle.Low);
+                    }
 
-                    ////static IEnumerable<Candle> Slice(SortedCandles source, int from, int to)
-                    ////{
-                    ////    for (var i = from; i <= to; i++)
-                    ////    {
-                    ////        yield return source[i];
-                    ////    }
-                    ////}
+                    static IEnumerable<Candle> Slice(SortedCandles source, int from, int to)
+                    {
+                        for (var i = from; i <= to; i++)
+                        {
+                            yield return source[i];
+                        }
+                    }
                 }
             }
 
