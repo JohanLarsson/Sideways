5bd984d012a9f806afd131564e62588d2f7d6e4d
JohanLarsson <johan_gavle1@hotmail.com>
2021-06-12 09:52:08 +0200

    Change interval in intraday charts.
    #29

---
+++ Sideways/Wpf/Converters/CandleIntervalToBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace Sideways
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public sealed class CandleIntervalToBoolConverter : IValueConverter
+    {
+        public static CandleIntervalToBoolConverter Hour = new(CandleInterval.Hour);
+        public static CandleIntervalToBoolConverter Minute = new(CandleInterval.Minute);
+
+        private readonly object trueWhen;
+
+        public CandleIntervalToBoolConverter(CandleInterval trueWhen)
+        {
+            this.trueWhen = trueWhen;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Equals(value, this.trueWhen);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is true ? this.trueWhen : Binding.DoNothing;
+        }
+    }
+}
