96d3d5d8fc4975e461ea1319c56a1dca7f1d672d
JohanLarsson <johan_gavle1@hotmail.com>
2021-06-22 17:57:08 +0200

    EarningsBar WIP

---
+++ Sideways/Chart/EarningsBar.cs
@@ -0,0 +1,13 @@
+﻿namespace Sideways
+{
+    using System.Windows;
+    using System.Windows.Documents;
+
+    public class EarningsBar : CandleSeries
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return new(25, TextElement.GetFontSize(this) * TextElement.GetFontFamily(this).LineSpacing);
+        }
+    }
+}
