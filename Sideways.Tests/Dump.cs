170332c2b5b2d59806543323a5724852e972691a
Johan Larsson <johan_gavle1@hotmail.com>
2021-06-30 16:08:27 +0200

    Dump

---
+++ Sideways.Tests/Dump.cs
@@ -0,0 +1,18 @@
+﻿namespace Sideways.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    public static class Dump
+    {
+        [Explicit]
+        [Test]
+        public static void Days()
+        {
+            foreach (var day in Database.ReadDays("DPLS", new DateTimeOffset(2020, 12, 01, 00, 00, 00, TimeSpan.Zero), new DateTimeOffset(2021, 03, 01, 00, 00, 00, TimeSpan.Zero)))
+            {
+                Console.WriteLine($"{day.Open} {day.Low} {day.High} {day.Close}");
+            }
+        }
+    }
+}
