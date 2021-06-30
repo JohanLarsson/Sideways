fa09877fac2aafb039841247370f621a7f49e2ef
JohanLarsson <johan_gavle1@hotmail.com>
2021-06-30 17:54:01 +0200

    Percent

---
+++ Sideways/Internal/Percent.cs
@@ -0,0 +1,49 @@
+﻿// ReSharper disable CompareOfFloatsByEqualityOperator
+namespace Sideways
+{
+    using System;
+
+    public readonly struct Percent : IEquatable<Percent>, IComparable<Percent>, IComparable
+    {
+        private readonly float value;
+
+        public Percent(float value)
+        {
+            this.value = value;
+        }
+
+        public static bool operator ==(Percent left, Percent right) => left.value == right.value;
+
+        public static bool operator !=(Percent left, Percent right) => left.value != right.value;
+
+        public static bool operator <(Percent left, Percent right) => left.value < right.value;
+
+        public static bool operator >(Percent left, Percent right) => left.value > right.value;
+
+        public static bool operator <=(Percent left, Percent right) => left.value <= right.value;
+
+        public static bool operator >=(Percent left, Percent right) => left.value >= right.value;
+
+        public static Percent From(float before, float after) => new((after - before) / Math.Abs(before));
+
+        public override string ToString() => $"{this.value}%";
+
+        public bool Equals(Percent other) => this.value == other.value;
+
+        public override bool Equals(object? obj) => obj is Percent other && this.Equals(other);
+
+        public override int GetHashCode() => this.value.GetHashCode();
+
+        public int CompareTo(Percent other) => this.value.CompareTo(other.value);
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is null)
+            {
+                return 1;
+            }
+
+            return obj is Percent other ? this.CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(Percent)}");
+        }
+    }
+}
