26ee24fa287c742dfef96e29460ebda9fe1b115d
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-01 17:19:56 +0200

    Center earnings tooltip.

---
+++ Sideways/Wpf/Converters/MultiplyConverterExtension.cs
@@ -0,0 +1,37 @@
+﻿namespace Sideways
+{
+    using System;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+
+    [ValueConversion(typeof(double), typeof(double))]
+    [MarkupExtensionReturnType(typeof(MultiplyConverterExtension))]
+    public sealed class MultiplyConverterExtension : MarkupExtension, IValueConverter
+    {
+        private readonly double factor;
+
+        public MultiplyConverterExtension(double factor)
+        {
+            this.factor = factor;
+        }
+
+        public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+        {
+            return value switch
+            {
+                double x => x * this.factor,
+                float x => x * this.factor,
+                int x => x * this.factor,
+                null => null,
+                _ => throw new NotSupportedException($"Not handling value of type {value.GetType().Name}"),
+            };
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException($"{nameof(SurpriseToVisibilityConverterExtension)} can only be used in OneWay bindings");
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
