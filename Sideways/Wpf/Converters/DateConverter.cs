16514909c038024246f383d2f2b87e575e50fcd7
Johan Larsson <johan_gavle1@hotmail.com>
2021-07-01 08:12:21 +0200

    Fix binding of dates.

---
+++ Sideways/Wpf/Converters/DateConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace Sideways
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(DateTime), typeof(DateTimeOffset))]
+    [ValueConversion(typeof(DateTimeOffset), typeof(DateTime))]
+    public sealed class DateConverter : IValueConverter
+    {
+        public static readonly DateConverter Default = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value switch
+            {
+                DateTimeOffset d => d.DateTime,
+                DateTime d => new DateTimeOffset(d),
+                null => null,
+                _ => throw new NotSupportedException($"Unknown value: {value}"),
+            };
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value switch
+            {
+                DateTimeOffset d => d.DateTime,
+                DateTime d => new DateTimeOffset(d),
+                null => null,
+                _ => throw new NotSupportedException($"Unknown value: {value}"),
+            };
+        }
+    }
+}
