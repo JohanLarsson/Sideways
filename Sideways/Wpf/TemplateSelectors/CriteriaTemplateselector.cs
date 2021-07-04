f08165e1f4e0191d1481192413894c58ac8b7dea
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-04 11:44:42 +0200

    Suppress warning.

---
+++ Sideways/Wpf/TemplateSelectors/CriteriaTemplateselector.cs
@@ -0,0 +1,22 @@
+﻿namespace Sideways
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Markup;
+
+    [ContentProperty(nameof(Templates))]
+    public class CriteriaTemplateselector : DataTemplateSelector
+    {
+#pragma warning disable CA1002 // Do not expose generic lists
+        public List<DataTemplate> Templates { get; } = new();
+#pragma warning restore CA1002 // Do not expose generic lists
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return this.Templates.SingleOrDefault(x => Equals(x.DataType, item?.GetType())) ??
+                   base.SelectTemplate(item, container);
+        }
+    }
+}
