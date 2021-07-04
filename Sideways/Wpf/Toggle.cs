647d93d51bf9e83cda24f2816dd71d20edcb05f6
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-04 09:49:56 +0200

    Toggle context buttons.

--- Sideways/Wpf/Toggle.cs
+++ Sideways/Wpf/Toggle.cs
@@ -9,17 +9,24 @@
             "IsSelected",
             typeof(bool),
             typeof(Toggle),
-            new PropertyMetadata(default(bool)));
+            new PropertyMetadata(false));
+
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.RegisterAttached(
+            "IsChecked",
+            typeof(bool),
+            typeof(Toggle),
+            new PropertyMetadata(false));
 
         static Toggle()
         {
             EventManager.RegisterClassHandler(typeof(DataGridCell), UIElement.PreviewMouseLeftButtonDownEvent, new RoutedEventHandler(OnPreviewMouseLeftButtonDown));
+            EventManager.RegisterClassHandler(typeof(RadioButton), UIElement.PreviewMouseLeftButtonDownEvent, new RoutedEventHandler(OnPreviewMouseLeftButtonDown));
 
             static void OnPreviewMouseLeftButtonDown(object sender, RoutedEventArgs e)
             {
                 switch (sender)
                 {
-                    case DataGridCell { IsSelected: true, IsEditing: false, Column: { } column } cell
+                    case DataGridCell { IsSelected: true, IsEnabled: true, IsEditing: false, Column: { } column } cell
                         when GetIsSelected(column) &&
                              cell.FirstAncestor<DataGridRow>() is { } row &&
                              row.FirstAncestor<DataGrid>() is { } grid:
@@ -35,6 +42,10 @@
 
                         e.Handled = true;
                         break;
+                    case RadioButton { IsChecked: true, IsEnabled: true } radioButton:
+                        radioButton.IsChecked = false;
+                        e.Handled = true;
+                        break;
                 }
             }
         }
@@ -43,5 +54,10 @@
         public static bool GetIsSelected(DependencyObject element) => (bool)element.GetValue(IsSelectedProperty);
 
         public static void SetIsSelected(DependencyObject element, bool value) => element.SetValue(IsSelectedProperty, value);
+
+        [AttachedPropertyBrowsableForType(typeof(RadioButton))]
+        public static bool GetIsChecked(DependencyObject element) => (bool)element.GetValue(IsCheckedProperty);
+
+        public static void SetIsChecked(DependencyObject element, bool value) => element.SetValue(IsCheckedProperty, value);
     }
 }
