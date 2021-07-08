1ad052956501bc7ebcc92e124055d2d7b1e1f650
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-08 09:11:03 +0200

    Suppress warnings.

--- Sideways/Internal/ReadOnlyListDebugView.cs
+++ Sideways/Internal/ReadOnlyListDebugView.cs
@@ -1,10 +1,13 @@
-﻿namespace Sideways
+﻿#pragma warning disable GU0073 // Member of non-public type should be internal.
+namespace Sideways
 {
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
 
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes
     internal sealed class ReadOnlyListDebugView<T>
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
     {
         private readonly IReadOnlyList<T> collection;
 
