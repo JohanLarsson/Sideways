685acbeeade9c920cc5faa50d6a3589ff52f3591
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-11 09:54:45 +0200

    Remove unused.

--- Sideways/Internal/EnumerableExtensions.cs
+++ Sideways/Internal/EnumerableExtensions.cs
@@ -71,46 +71,5 @@
                 yield return merged;
             }
         }
-
-        public static T? FirstOrNull<T>(this IEnumerable<T> xs)
-            where T : struct
-        {
-            using var e = xs.GetEnumerator();
-            if (e.MoveNext())
-            {
-                return e.Current;
-            }
-
-            return null;
-        }
-
-        public static T? FirstOrNull<T>(this IEnumerable<T> xs, Func<T, bool> selector)
-            where T : struct
-        {
-            foreach (var x in xs)
-            {
-                if (selector(x))
-                {
-                    return x;
-                }
-            }
-
-            return null;
-        }
-
-        public static T? LastOrNull<T>(this IEnumerable<T> xs, Func<T, bool> selector)
-            where T : struct
-        {
-            T? match = null;
-            foreach (var x in xs)
-            {
-                if (selector(x))
-                {
-                    match = x;
-                }
-            }
-
-            return match;
-        }
     }
 }
