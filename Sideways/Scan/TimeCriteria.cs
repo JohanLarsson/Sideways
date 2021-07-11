79ae1d642fb15e37b4cbddf271513ca0da798f60
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-11 06:11:49 +0200

    More predefined time criteria

--- Sideways/Scan/TimeCriteria.cs
+++ Sideways/Scan/TimeCriteria.cs
@@ -1,27 +1,40 @@
 ﻿namespace Sideways.Scan
 {
     using System;
+    using System.Globalization;
 
     public sealed class TimeCriteria : Criteria
     {
         private DateTimeOffset? start = DateTimeOffset.Now.Date.AddMonths(-1);
         private DateTimeOffset? end;
 
+        public static string Year1Text => (DateTime.Today.Year - 1).ToString(CultureInfo.InvariantCulture);
+
+        public static string Year2Text => (DateTime.Today.Year - 2).ToString(CultureInfo.InvariantCulture);
+
+        public static string Year3Text => (DateTime.Today.Year - 3).ToString(CultureInfo.InvariantCulture);
+
+        public static string Year4Text => (DateTime.Today.Year - 4).ToString(CultureInfo.InvariantCulture);
+
         public override string Info => this switch
+        {
+            { YearToDate: true } => "YTD",
+            { LastMonth: true } => "Month",
+            { LastWeek: true } => "Week",
+            { Year1: true } => Year1Text,
+            { Year2: true } => Year2Text,
+            { Year3: true } => Year3Text,
+            { Year4: true } => Year4Text,
+            _ => (this.Start, this.End) switch
             {
-                { YearToDate: true } => "YTD",
-                { LastMonth: true } => "Last month",
-                { LastWeek: true } => "Last week",
-                _ => (this.Start, this.End) switch
-                {
-                    // ReSharper disable LocalVariableHidesMember
-                    (Start: { } start, End: { } end) => $"[{start:yyyy-MM-dd}..{end:yyyy-MM-dd}]",
-                    (Start: null, End: { } end) => $"[..{end:yyyy-MM-dd}]",
-                    (Start: { } start, End: null) => $"[{start:yyyy-MM-dd}..]",
-                    (null, null) => "Time *",
-                    //// ReSharper restore LocalVariableHidesMember
-                },
-            };
+                // ReSharper disable LocalVariableHidesMember
+                (Start: { } start, End: { } end) => $"[{start:yyyy-MM-dd}..{end:yyyy-MM-dd}]",
+                (Start: null, End: { } end) => $"[..{end:yyyy-MM-dd}]",
+                (Start: { } start, End: null) => $"[{start:yyyy-MM-dd}..]",
+                (null, null) => "Time *",
+                //// ReSharper restore LocalVariableHidesMember
+            },
+        };
 
         // ReSharper disable once InconsistentNaming
         public bool YearToDate
@@ -72,6 +85,70 @@
             }
         }
 
+        public bool Year1
+        {
+            get => this.start == FirstDayOfYear(DateTime.Today.Year - 1) && this.end == LastDayOfYear(DateTime.Today.Year - 1);
+            set
+            {
+                if (!value ||
+                    value == this.Year1)
+                {
+                    return;
+                }
+
+                this.Start = FirstDayOfYear(DateTime.Today.Year - 1);
+                this.End = LastDayOfYear(DateTime.Today.Year - 1);
+            }
+        }
+
+        public bool Year2
+        {
+            get => this.start == FirstDayOfYear(DateTime.Today.Year - 2) && this.end == LastDayOfYear(DateTime.Today.Year - 2);
+            set
+            {
+                if (!value ||
+                    value == this.Year2)
+                {
+                    return;
+                }
+
+                this.Start = FirstDayOfYear(DateTime.Today.Year - 2);
+                this.End = LastDayOfYear(DateTime.Today.Year - 2);
+            }
+        }
+
+        public bool Year3
+        {
+            get => this.start == FirstDayOfYear(DateTime.Today.Year - 3) && this.end == LastDayOfYear(DateTime.Today.Year - 3);
+            set
+            {
+                if (!value ||
+                    value == this.Year3)
+                {
+                    return;
+                }
+
+                this.Start = FirstDayOfYear(DateTime.Today.Year - 3);
+                this.End = LastDayOfYear(DateTime.Today.Year - 3);
+            }
+        }
+
+        public bool Year4
+        {
+            get => this.start == FirstDayOfYear(DateTime.Today.Year - 4) && this.end == LastDayOfYear(DateTime.Today.Year - 4);
+            set
+            {
+                if (!value ||
+                    value == this.Year4)
+                {
+                    return;
+                }
+
+                this.Start = FirstDayOfYear(DateTime.Today.Year - 4);
+                this.End = LastDayOfYear(DateTime.Today.Year - 4);
+            }
+        }
+
         public DateTimeOffset? Start
         {
             get => this.start;
@@ -117,5 +194,7 @@
         }
 
         private static DateTimeOffset FirstDayOfYear(int year) => new(year, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
+
+        private static DateTimeOffset LastDayOfYear(int year) => new(year, 12, 31, 0, 0, 0, 0, TimeSpan.Zero);
     }
 }
