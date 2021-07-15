7ee540f84d8fbf03606dc3cd46b79059d67803be
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-15 15:33:17 +0200

    MinutesDownloadTests

---
+++ Sideways.Tests/AlphaVantage/MinutesDownloadTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Sideways.Tests.AlphaVantage
+{
+    using System;
+    using System.Collections.Immutable;
+    using NUnit.Framework;
+
+    using Sideways.AlphaVantage;
+
+    public static class MinutesDownloadTests
+    {
+        private static readonly AlphaVantageClientSettings AlphaVantageClientSettings = new()
+        {
+            ApiKey = "apiKey",
+            MaxCallsPerMinute = 5,
+        };
+
+        [Test]
+        public static void WhenSymbolsWithMissingMinutes()
+        {
+            var settings = new AlphaVantageSettings(
+                AlphaVantageClientSettings,
+                symbolsWithMissingMinutes: ImmutableSortedSet.Create("ACHN"),
+                unlistedSymbols: ImmutableSortedSet<string>.Empty,
+                firstMinutes: ImmutableSortedDictionary<string, DateTimeOffset>.Empty);
+            CollectionAssert.IsEmpty(MinutesDownload.Create("ACHN", default, default, null, settings));
+        }
+    }
+}
