4a22dc475834baecd54645da5c58a8e89bed214a
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-15 17:48:01 +0200

    Script for downloading holes in minutes.
    Fixup for bug.

--- Sideways.Tests/AlphaVantage/Download.cs
+++ Sideways.Tests/AlphaVantage/Download.cs
@@ -1,10 +1,11 @@
 ﻿namespace Sideways.Tests.AlphaVantage
 {
+    using System;
     using System.Collections.Generic;
+    using System.Collections.Immutable;
     using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
-
     using NUnit.Framework;
 
     using Sideways.AlphaVantage;
@@ -12,8 +13,10 @@
     [Explicit]
     public static class Download
     {
-        private static readonly AlphaVantageClientSettings ClientSettings = Settings.FromFile().AlphaVantage.ClientSettings;
-        private static readonly Downloader Downloader = new(Settings.FromFile());
+        private static readonly Settings Settings = Settings.FromFile();
+        private static readonly AlphaVantageClientSettings ClientSettings = Settings.AlphaVantage.ClientSettings;
+        private static readonly Downloader Downloader = new(Settings);
+        private static readonly ImmutableArray<string> Symbols = Database.ReadSymbols();
 
         [Test]
         public static async Task Listings()
@@ -28,6 +31,41 @@
         {
             var daysAndSplitsAsync = await Downloader.DaysAndSplitsAsync(symbol);
             CollectionAssert.IsNotEmpty(daysAndSplitsAsync.Candles);
+        }
+
+        [TestCaseSource(nameof(Symbols))]
+        public static async Task MinuteHoles(string symbol)
+        {
+            var days = Database.ReadDays(symbol, DateTimeOffset.Now.AddYears(-2), DateTimeOffset.Now.AddDays(-29)).Select(x => x.Time.Date);
+            var minuteDays = Database.ReadMinutes(symbol, DateTimeOffset.Now.AddYears(-2), DateTimeOffset.Now.AddDays(-29)).Select(x => x.Time.Date).Distinct();
+            if (minuteDays.Any() &&
+                days.SequenceEqual(minuteDays))
+            {
+                foreach (var slice in Enum.GetValues<Slice>().Where(x => x == Slice.Year1Month1))
+                {
+                    if (ShouldDownload(slice))
+                    {
+                        var minutesDownload = new MinutesDownload(symbol, slice, Downloader);
+                        await minutesDownload.ExecuteAsync();
+                    }
+
+                    bool ShouldDownload(Slice slice)
+                    {
+                        var sliceRange = TimeRange.FromSlice(slice);
+                        return SliceDays(days).SequenceEqual(SliceDays(minuteDays));
+
+                        IEnumerable<DateTime> SliceDays(IEnumerable<DateTime> source)
+                        {
+                            var firstMinute = Settings.AlphaVantage.FirstMinutes.GetValueOrDefault(symbol);
+                            return source.Where(x => sliceRange.Contains(x) && x > firstMinute);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Assert.Pass("No holes!");
+            }
         }
 
         [TestCase("TSLA")]
@@ -47,7 +85,7 @@
             return Database.ReadListings()
                 .Select(x => x.Symbol)
                 .Where(x => x.Length is > 1 and < 5 && !x.Contains("-"))
-                .Except(Database.ReadSymbols())
+                .Except(Symbols)
                 .Take(500);
         }
     }
