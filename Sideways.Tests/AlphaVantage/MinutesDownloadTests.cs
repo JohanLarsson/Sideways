3d62b7454c26e21652fb36b75f01051c7d03c63b
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-15 16:04:17 +0200

    Test minute download when unlisted complete

--- Sideways.Tests/AlphaVantage/MinutesDownloadTests.cs
+++ Sideways.Tests/AlphaVantage/MinutesDownloadTests.cs
@@ -22,7 +22,20 @@
                 symbolsWithMissingMinutes: ImmutableSortedSet.Create("ACHN"),
                 unlistedSymbols: ImmutableSortedSet<string>.Empty,
                 firstMinutes: ImmutableSortedDictionary<string, DateTimeOffset>.Empty);
-            CollectionAssert.IsEmpty(MinutesDownload.Create("ACHN", default, default, null, settings));
+            CollectionAssert.IsEmpty(MinutesDownload.Create("ACHN", default, default, null!, settings));
+        }
+
+        [Test]
+        public static void WhenUnlistedComplete()
+        {
+            var settings = new AlphaVantageSettings(
+                AlphaVantageClientSettings,
+                symbolsWithMissingMinutes: ImmutableSortedSet<string>.Empty,
+                unlistedSymbols: ImmutableSortedSet.Create("I"),
+                firstMinutes: ImmutableSortedDictionary<string, DateTimeOffset>.Empty);
+            var existingDays = new TimeRange(new DateTimeOffset(2013, 4, 18, 0, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2020, 5, 18, 0, 0, 0, 0, TimeSpan.Zero));
+            var existingMinutes = new TimeRange(new DateTimeOffset(2018, 4, 18, 0, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2020, 5, 18, 0, 0, 0, 0, TimeSpan.Zero));
+            CollectionAssert.IsEmpty(MinutesDownload.Create("I", existingDays, existingMinutes, null!, settings));
         }
     }
 }
