b8c45a91ec310fc69da233bb4298fd96e6329250
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-16 14:58:40 +0200

    Tweak optimial slice utilization prio.

--- Sideways/AlphaVantage/Downloader.cs
+++ Sideways/AlphaVantage/Downloader.cs
@@ -324,7 +324,7 @@
 
                 static int FullMinuteDownload(SymbolDownloads x)
                 {
-                    return (DateTimeOffset.Now - x.ExistingMinutes.Max).Days is >= 28 and <= 30 ? 0 : 1;
+                    return (DateTimeOffset.Now - x.ExistingMinutes.Max).Days % 30 < 2 ? 0 : 1;
                 }
             }
         }
