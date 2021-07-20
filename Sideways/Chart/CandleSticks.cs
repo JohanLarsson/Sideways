4924c8abed47bbe501c765ae4574aad303c019b0
JohanLarsson <johan_gavle1@hotmail.com>
2021-07-20 14:21:20 +0200

    Simplify: draw candles using DrawingContext
    Profiler indicates visual is slower.

--- Sideways/Chart/CandleSticks.cs
+++ Sideways/Chart/CandleSticks.cs
@@ -21,16 +21,9 @@
         private static readonly Pen IncreasingPen = Brushes.CreatePen(Brushes.Increasing);
         private static readonly Pen DecreasingPen = Brushes.CreatePen(Brushes.Decreasing);
 
-        private readonly LayerVisual layer = new();
-
         static CandleSticks()
         {
             RenderOptions.EdgeModeProperty.OverrideMetadata(typeof(CandleSticks), new UIPropertyMetadata(EdgeMode.Aliased));
-        }
-
-        public CandleSticks()
-        {
-            this.AddVisualChild(this.layer);
         }
 
         public FloatRange? PriceRange
@@ -45,15 +38,8 @@
             set => this.SetValue(PriceScaleProperty, value);
         }
 
-        protected override int VisualChildrenCount => 1;
-
-        protected override Visual GetVisualChild(int index) => index == 0
-            ? this.layer
-            : throw new ArgumentOutOfRangeException(nameof(index));
-
         protected override void OnRender(DrawingContext drawingContext)
         {
-            using var context = this.layer.RenderOpen();
             if (this.PriceRange is { } range)
             {
                 var position = CandlePosition.RightToLeftPadded(this.RenderSize, this.CandleWidth, new ValueRange(range, this.PriceScale));
@@ -61,7 +47,7 @@
                 {
                     if (candle is { Volume: > 0, Low: > 0 })
                     {
-                        context.DrawLine(
+                        drawingContext.DrawLine(
                             GetPen(candle),
                             new Point(position.Center, position.Y(candle.Low)),
                             new Point(position.Center, position.Y(candle.High)));
@@ -72,7 +58,7 @@
                             yClose += 1;
                         }
 
-                        context.DrawRectangle(
+                        drawingContext.DrawRectangle(
                             Brushes.Get(candle),
                             null,
                             Rect(
